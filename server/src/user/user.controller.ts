0d2f7fdef0f3695da36a91ca47ede8fefb85c133
Kiptoo Magutt <kiptoo@twende.app>
2022-12-07 18:00:36 +0300

    add custom logic to user service & controller
    - testing amp based on the 2 docs:
        - https://docs.amplication.com/custom-code/business-logic/
        - https://docs.amplication.com/custom-code/controller-action/

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -4,6 +4,11 @@ import * as nestAccessControl from "nest-access-control";
 import { UserService } from "./user.service";
 import { UserControllerBase } from "./base/user.controller.base";
 
+import * as errors from "../errors";
+import { User } from "./base/User";
+import { UserWhereUniqueInput } from "./base/UserWhereUniqueInput";
+import { AclValidateRequestInterceptor } from "../interceptors/aclValidateRequest.interceptor";
+
 @swagger.ApiTags("users")
 @common.Controller("users")
 export class UserController extends UserControllerBase {
@@ -14,4 +19,29 @@ export class UserController extends UserControllerBase {
   ) {
     super(service, rolesBuilder);
   }
+
+  @common.UseInterceptors(AclValidateRequestInterceptor)
+  @common.Patch("/:id/password")
+  @nestAccessControl.UseRoles({
+    resource: "User",
+    action: "update",
+    possession: "own",
+  })
+  @swagger.ApiOkResponse({ type: User })
+  @swagger.ApiNotFoundResponse({ type: errors.NotFoundException })
+  @swagger.ApiForbiddenResponse({ type: errors.ForbiddenException })
+  async resetPassword(
+    @common.Param() params: UserWhereUniqueInput,
+  ): Promise<User | null> {
+    const result = await this.service.resetPassword({
+      where: params,
+    });
+    if (result === null) {
+      throw new errors.NotFoundException(
+        `No resource was found for ${JSON.stringify(params)}`
+      );
+    }
+    return result;
+  }
+
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -3,6 +3,8 @@ import { PrismaService } from "nestjs-prisma";
 import { UserServiceBase } from "./base/user.service.base";
 import { PasswordService } from "../auth/password.service";
 
+import { User } from "./base/User";
+import { UserFindUniqueArgs } from "./base/UserFindUniqueArgs";
 @Injectable()
 export class UserService extends UserServiceBase {
   constructor(
@@ -10,5 +12,14 @@ export class UserService extends UserServiceBase {
     protected readonly passwordService: PasswordService
   ) {
     super(prisma, passwordService);
+  }
+
+  async resetPassword(args: UserFindUniqueArgs): Promise<User> {
+    return this.prisma.user.update({
+      where: args.where,
+      data:{
+        password:"123456"
+      }
+    });
   }
 }
